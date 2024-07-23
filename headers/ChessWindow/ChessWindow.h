cefdd4d138ca2b9d3e87a6c5f308101f77208f3c
Ismael Cuevas <ijcuevas2@gmail.com>
2024-07-23 05:02:49 -0500

    wip: showing top menu

--- headers/ChessWindow/ChessWindow.h
+++ headers/ChessWindow/ChessWindow.h
@@ -10,6 +10,14 @@
 class ChessWindow : public Gtk::Window {
 public:
     ChessWindow();
+protected:
+    // Child widgets
+    void on_menu_new_game();
+    void on_menu_quit();
+    Gtk::Box m_box;
+    Glib::RefPtr<Gio::Menu> m_menu;
+    Gtk::PopoverMenu m_popover_menu;
+    Gtk::MenuButton m_menu_button;
 private:
     ChessBoardView* m_chessBoardView;
 };
