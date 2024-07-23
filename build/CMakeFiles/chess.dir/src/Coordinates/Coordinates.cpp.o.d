44f8ed33197f8d2afb1b9d83e07246854416c905
Ismael Cuevas <ijcuevas2@gmail.com>
2024-07-22 19:54:05 -0500

    adding build files

---
+++ build/CMakeFiles/chess.dir/src/Coordinates/Coordinates.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/chess.dir/src/Coordinates/Coordinates.cpp.o: \
+  /Users/ismael/Documents/chess-gtkmm4/src/Coordinates/Coordinates.cpp \
+  /Users/ismael/Documents/chess-gtkmm4/src/Coordinates/../../headers/Coordinates/Coordinates.h
---
+++ build/CMakeFiles/chess_resources.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess_resources"
+  "chess_resources.c"
+  "chess_resources.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/chess_resources.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
