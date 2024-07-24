471d2fa467deefea5a9377bb9be95bb728308f02
Ismael Cuevas <ijcuevas2@gmail.com>
2024-07-24 03:45:10 -0500

    Adds behavior for making a new game and quitting the program

--- headers/ChessBoard/ChessBoardView.h
+++ headers/ChessBoard/ChessBoardView.h
@@ -4,19 +4,30 @@
 
 #ifndef CHESS_CHESSBOARDVIEW_H
 #define CHESS_CHESSBOARDVIEW_H
+
 #include <gtkmm.h>
 #include "ChessBoardModel.h"
 #include "../ChessImagesInfo/ChessImagesInfo.h"
 #include "ChessBoard/ChessBoardController.h"
 
-class ChessBoardView : public Gtk::DrawingArea {
+class ChessBoardView : public Gtk::Box {
 public:
     ChessBoardView();
+
+private:
     void on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height);
     void on_pressed(int n_press, double x, double y);
-private:
-    ChessBoardController chessBoardController;
-};
+    void on_new_game_clicked();
+    void on_exit_clicked();
+    void initBoard();
+    void clearBoard();
 
+    ChessBoardController chessBoardController;
+    Gtk::DrawingArea m_drawingArea;
+    Glib::RefPtr<Gio::Menu> m_menuModel;
+    Gtk::PopoverMenuBar* m_menuBar;
+    Glib::RefPtr<Gio::SimpleAction> m_newGameAction;
+    Glib::RefPtr<Gio::SimpleAction> m_exitAction;
+};
 
 #endif //CHESS_CHESSBOARDVIEW_H
--- src/ChessBoard/ChessBoardView.cpp
+++ src/ChessBoard/ChessBoardView.cpp
@@ -4,14 +4,49 @@
 
 #include "../../headers/ChessBoard/ChessBoardView.h"
 
-ChessBoardView::ChessBoardView() {
-  set_content_width(450);
-  set_content_height(450);
-  set_draw_func(sigc::mem_fun(*this, &ChessBoardView::on_draw));
-  add_controller(Gtk::GestureClick::create());
+ChessBoardView::ChessBoardView() : Gtk::Box(Gtk::Orientation::VERTICAL) {
+  m_drawingArea.set_content_width(450);
+  m_drawingArea.set_content_height(450);
+  m_drawingArea.set_draw_func(sigc::mem_fun(*this, &ChessBoardView::on_draw));
+
   auto controller = Gtk::GestureClick::create();
   controller->signal_pressed().connect(sigc::mem_fun(*this, &ChessBoardView::on_pressed));
-  add_controller(controller);
+  m_drawingArea.add_controller(controller);
+
+  // Create the menu model
+  m_menuModel = Gio::Menu::create();
+  auto fileMenu = Gio::Menu::create();
+  m_menuModel->append_submenu("File", fileMenu);
+
+  // Create actions
+  m_newGameAction = Gio::SimpleAction::create("new_game");
+  m_exitAction = Gio::SimpleAction::create("exit");
+
+  // Connect action signals using lambda functions
+  m_newGameAction->signal_activate().connect([this](const Glib::VariantBase&) {
+      this->on_new_game_clicked();
+  });
+  m_exitAction->signal_activate().connect([this](const Glib::VariantBase&) {
+      this->on_exit_clicked();
+  });
+
+  // Add actions to the application
+  auto app = Gtk::Application::get_default();
+  app->add_action(m_newGameAction);
+  app->add_action(m_exitAction);
+
+  // Add menu items
+  fileMenu->append("New Game", "app.new_game");
+  fileMenu->append("Exit", "app.exit");
+
+  // Create the menu bar
+  m_menuBar = Gtk::make_managed<Gtk::PopoverMenuBar>(m_menuModel);
+
+  // Add the menu bar to the box
+  append(*m_menuBar);
+
+  // Add the drawing area to the box
+  append(m_drawingArea);
 }
 
 void ChessBoardView::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height) {
@@ -19,9 +54,30 @@ void ChessBoardView::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width,
 }
 
 void ChessBoardView::on_pressed(int n_press, double x, double y) {
-  int height = get_height();
-  int width = get_width();
+  int height = m_drawingArea.get_height();
+  int width = m_drawingArea.get_width();
   chessBoardController.on_pressed(n_press, x, y, width, height);
-  queue_draw();
+  m_drawingArea.queue_draw();
 }
 
+void ChessBoardView::initBoard() {
+  chessBoardController.initBoard();
+}
+
+void ChessBoardView::clearBoard() {
+  chessBoardController.clearBoard();
+}
+
+void ChessBoardView::on_new_game_clicked() {
+  // Handle new game action
+  // Implement your new game logic here
+  clearBoard();
+  initBoard();
+  m_drawingArea.queue_draw();
+}
+
+void ChessBoardView::on_exit_clicked() {
+  // Handle exit action
+  auto app = Gtk::Application::get_default();
+  app->quit();
+}
