263e0a1c50af3509280630edb47c8da1ed64766c
Ismael Cuevas <ijcuevas2@gmail.com>
2024-08-18 16:31:44 -0500

    removing build dir

--- build/CMakeFiles/chess.dir/src/Coordinates.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/chess.dir/src/Point2DPair.cpp.o: \
-  /Users/ismael/Documents/chess-gtkmm4/src/Point2DPair.cpp \
-  /Users/ismael/Documents/chess-gtkmm4/src/../headers/Point2DPair.h
--- build/CMakeFiles/chess.dir/src/Coordinates/Coordinates.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/chess.dir/src/Point2DPair/Point2DPair.cpp.o: \
-  /Users/ismael/Documents/chess-gtkmm4/src/Point2DPair/Point2DPair.cpp \
-  /Users/ismael/Documents/chess-gtkmm4/src/Point2DPair/../../headers/Point2DPair/Point2DPair.h
