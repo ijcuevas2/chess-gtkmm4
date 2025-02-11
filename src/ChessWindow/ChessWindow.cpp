dd5ee3df36889acccff75d6610d7e98006b95832
Ismael Cuevas <ijcuevas2@gmail.com>
2025-02-11 10:02:37 -0600

    Saving return value to variable

--- src/ChessWindow/ChessWindow.cpp
+++ src/ChessWindow/ChessWindow.cpp
@@ -107,7 +107,8 @@ void ChessWindow::openFileDialog() {
   Glib::RefPtr<Gio::ListStore<Gtk::FileFilter>> filters = Gio::ListStore<Gtk::FileFilter>::create();
   std::string chessSavesDir = "chess_saves";
   fs::path currentPath = fs::current_path() / chessSavesDir;
-  Glib::RefPtr<Gio::File> initial_folder = Gio::File::create_for_path(currentPath.string());
+  std::string pathString = currentPath.string();
+  Glib::RefPtr<Gio::File> initial_folder = Gio::File::create_for_path(pathString);
   dialog->set_initial_folder(initial_folder);
 
   Glib::RefPtr<Gtk::FileFilter> filter_any = Gtk::FileFilter::create();
